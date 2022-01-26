0596f33b49129f3ed0ce8e53dccafd56edf534eb
HenriJarvoja <91941305+HenriJarvoja@users.noreply.github.com>
2022-01-26 14:52:12 +0200

    Delete 1.6 Transport.py

--- 1.6 Transport.py
+++
@@ -1,7 +0,0 @@
-inimeste_arv = int(input("Sisestage inimeste arv: "))
-kohtade_arv = int(input("Sisestage kohtade arv ühes bussis: ")) 
-busside_arv = inimeste_arv // kohtade_arv
-mahajaanud = inimeste_arv % kohtade_arv
-if mahajaanud > 0:
-    busside_arv = busside_arv + 1
-print("Tuleb tellida " + str(busside_arv) + " bussi.")
