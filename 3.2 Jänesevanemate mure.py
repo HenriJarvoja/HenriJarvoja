2470242631c0b31748e5f7915ef1e0ec6faf3af0
HenriJarvoja <91941305+HenriJarvoja@users.noreply.github.com>
2022-01-26 14:51:54 +0200

    Delete 3.2 Jänesevanemate mure.py

--- 3.2 Jänesevanemate mure.py
+++
@@ -1,9 +0,0 @@
-ringide_arv = int(input("Sisestage ringide arv: "))
-porgandid_kokku = 0
-porgandite_arv_karbis = 0
-ring = 1
-while ring <= ringide_arv:
-    porgandite_arv_karbis = porgandite_arv_karbis + ring
-    porgandid_kokku = porgandid_kokku + porgandite_arv_karbis
-    ring = ring + 1
-print("porgandite koguarv on " + str(porgandid_kokku))
