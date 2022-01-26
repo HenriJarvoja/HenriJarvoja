1fd2c30a0e6181ef03bfdcd0cbfdb8a9e0af41a1
HenriJarvoja <91941305+HenriJarvoja@users.noreply.github.com>
2022-01-26 14:50:36 +0200

    Delete 1.5 Nädala ajakulu.py

--- 1.5 Nädala ajakulu.py
+++
@@ -1,5 +0,0 @@
-ainepunktide_arv = int(input("Sisestage ainepunktide arv: "))
-nadalate_arv = int(input("Sisestage nädalate arv: "))
-tunnid = ainepunktide_arv * 26
-nadalate_ajakulu = tunnid / nadalate_arv
-print(round(nadalate_ajakulu))
